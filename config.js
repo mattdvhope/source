df0614f2b57eb801c60bfa859334e0c3b7d1b746
mattdvhope <mattagape@me.com>
2020-07-21 06:24:51 +0700

    Changed some links

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = {
-  url: "https://7sferry-gatsby-contentful-starters.netlify.app",
+  url: "https://sourceofallwealth.com/",
   title: "Source of All Wealth",
   tagline: "Your tagline",
   description: `Explaining the Source of All Wealth.`,
@@ -10,11 +10,11 @@ module.exports = {
     name: "Matt Malone",
     realName: "Matthew Malone",
     contacts: {
-      linkedin: "https://www.linkedin.com/in/7sferry/",
-      github: "https://github.com/7sferry",
-      facebook: "https://facebook.com/7sferry",
+      linkedin: "https://www.linkedin.com/in/matthew-malone-06183a74/",
+      github: "https://github.com/",
+      facebook: "https://facebook.com/mattagape",
       crystal: "https://www.crystalknows.com/p/ferry",
-      blogger: "https://ferry.now.sh/",
+      blogger: "https://sourceofallwealth.com/",
       resume: "https://ferry.netlify.com/resume/",
     },
   },
