ec4fa74ea409897eec3ff32732d0f996195cac1d
Matt Malone <mattagape@me.com>
2021-01-20 15:32:03 +0700

    Added link

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import { Link } from "gatsby";
 
 export default class footer extends Component {
   render() {
     return (
       <div className="site-footer" id="footer">
         <div className="container">
-          <span>{this.props.siteName}</span>
+          <span><Link to="/" style={{ color: `#fff`}}>{this.props.siteName}</Link></span>
         </div>
       </div>
     );
