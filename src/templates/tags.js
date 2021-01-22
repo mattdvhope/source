19df714fcd6385597000fd703f8a33b5ec0aa36d
Matt Malone <mattagape@me.com>
2021-01-22 13:33:18 +0700

    Added tag page template

---
+++ src/templates/tags.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import { Link, graphql } from "gatsby"
+import Layout from "../components/layout";
+
+//<<<<< Page Template for '/tags/{tag}' >>>>>>>
+
+const Tags = ({ pageContext, data }) => {
+
+  console.log(pageContext);
+  console.log(data);
+
+  const { tag } = pageContext
+  const { edges, totalCount } = data.allContentfulSteps
+  const tagHeader = `${totalCount} post${
+    totalCount === 1 ? "" : "s"
+  } tagged with "${tag}"`
+
+  return (
+    <Layout>
+      <div>
+        <h1>{tagHeader}</h1>
+        <ul>
+          {edges.map(({ node }) => {
+            const { slug } = node
+            const { title } = node
+            return (
+              <li key={slug}>
+                <Link to={slug}>{title}</Link>
+              </li>
+            )
+          })}
+        </ul>
+        {/*
+                This links to a page that does not yet exist.
+                You'll come back to it!
+              */}
+        <Link to="/tags">All tags</Link>
+      </div>
+    </Layout>
+  )
+}
+
+export default Tags;
+
+export const pageQuery = graphql`
+  query TagsQuery($tag: String) {
+    allContentfulSteps(
+      limit: 2000
+      sort: { fields: [createdAt], order: DESC }
+      filter: { tags: {in: [$tag] } }
+    ) {
+      totalCount
+      edges  {
+        node {
+          title
+          slug
+        }
+      }
+    }
+    
+  }
+`
